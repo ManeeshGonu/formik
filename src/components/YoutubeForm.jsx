6a4608e9c7c35e9b613fe02db2c75439c266eb27
ManeeshGonu <gonumaneesh@gmail.com>
2024-12-24 00:30:36 +0530

    validate function

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -2,15 +2,40 @@ import React from "react";
 import { useFormik } from "formik";
 
 const YoutubeForm = () => {
+  const initialValues = {
+    name: "Maneesh",
+    email: "",
+    channel: "",
+  };
+
+  const onSubmit = (values) => {
+    console.log("Form Data", values);
+  };
+
+  const validate = (values) => {
+    //values.name, values.email, values.channel
+    //errors.name, errors.email, errors.channel
+
+    let errors = {};
+
+    if (!values.name) errors.name = "Required";
+    if (!values.name) {
+      errors.name = "Required";
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)
+    ) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!values.channel) errors.channel = "Required";
+
+    return errors;
+  };
+
   const formik = useFormik({
-    initialValues: {
-      name: "Maneesh",
-      email: "",
-      channel: "",
-    },
-    onSubmit: (values) => {
-      console.log("Form Data", values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   // console.log("Form Values", formik.values);
@@ -18,7 +43,7 @@ const YoutubeForm = () => {
   return (
     <form
       className="w-1/2 mx-auto min-h-screen flex flex-col justify-center items-center space-y-5"
-      onSubmit={formik.handleSubmit }
+      onSubmit={formik.handleSubmit}
     >
       <div className="w-1/2">
         <label htmlFor="name">Name</label>

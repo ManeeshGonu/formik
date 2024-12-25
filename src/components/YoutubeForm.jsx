613e823fd34e1f8e39144d6e273b3512e5b345cb
ManeeshGonu <gonumaneesh@gmail.com>
2024-12-25 17:14:30 +0530

    Implementation of field component with render props method

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -4,37 +4,23 @@ import * as Yup from "yup";
 
 const YoutubeForm = () => {
   const initialValues = {
-    name: "Maneesh",
+    name: "",
     email: "",
     channel: "",
+    comments: "",
+    address: "",
   };
 
   const onSubmit = (values) => {
     console.log("Form Data", values);
   };
-  //   //values.name, values.email, values.channel
-  //   //errors.name, errors.email, errors.channel
-
-  //   let errors = {};
-
-  //   if (!values.name) errors.name = "Required";
-  //   if (!values.email) {
-  //     errors.email = "Required";
-  //   } else if (
-  //     !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(values.email)
-  //   ) {
-  //     errors.email = "Invalid email format";
-  //   }
-
-  //   if (!values.channel) errors.channel = "Required";
-
-  //   return errors;
-  // };
 
   const validationSchema = Yup.object({
     name: Yup.string().required("Required!"),
     email: Yup.string().email("Invalid email format").required("Required!"),
     channel: Yup.string().required("Required!"),
+    address: Yup.string().required("Required!"),
+    comments: Yup.string().required("Required!"),
   });
 
   // console.log("Form Values", formik.values);
@@ -61,8 +47,40 @@ const YoutubeForm = () => {
           </div>
           <div className="w-full">
             <label htmlFor="channel">Channel</label>
-            <Field type="text" id="channel" name="channel" />
+            <Field
+              type="text"
+              id="channel"
+              name="channel"
+              placeholder="Channel name"
+            />
             <ErrorMessage name="channel" />
+          </div>
+          <div className="w-full">
+            <label htmlFor="comments">Comments</label>
+            <Field
+              as="textarea"
+              id="comments"
+              name="comments"
+              placeholder="comments"
+            />
+            <ErrorMessage name="comments" />
+          </div>
+
+          <div className="w-full">
+            <Field name="address">
+              {(props) => {
+                const { field, form, meta } = props;
+                return (
+                  <div>
+                    <label htmlFor="address">Address</label>
+                    <input id="address" {...field} />
+                    {meta.touched && meta.error ? (
+                      <div className="error">{meta.error}</div>
+                    ) : null}
+                  </div>
+                );
+              }}
+            </Field>
           </div>
           <button type="submit">Submit</button>
         </Form>

490229cfb21f632a3baa0788d9cc5d1fbbef75f6
ManeeshGonu <gonumaneesh@gmail.com>
2024-12-25 12:07:17 +0530

    validation with yup

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const YoutubeForm = () => {
   const initialValues = {
@@ -32,10 +33,16 @@ const YoutubeForm = () => {
     return errors;
   };
 
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required!"),
+    email: Yup.string().email("Invalid email format").required("Required!"),
+    channel: Yup.string().required("Required!"),
+  });
+
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
   });
 
   // console.log("Form Values", formik.values);
